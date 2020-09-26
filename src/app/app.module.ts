0148328c49e2039443652fb66d61dcf0298d9736
ShubhamThakral <sthakral071@gmail.com>
2020-09-26 14:15:45 +0530

    2nd Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginHeaderComponent } from './login/login-header/login-header.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
+import { SignupFormComponent } from './login/signup-form/signup-form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginHeaderComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
